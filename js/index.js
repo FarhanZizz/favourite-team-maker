afeefad3436b1b12aee83534bafc11d3fc97e55a
FarhanZizz <iamfarhanaziz@gmail.com>
2022-08-22 23:37:57 -0700

    Final Commit

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ function playerSelection(element) {
     const li = document.createElement("li");
     li.innerText = playerName;
     document.getElementById("selected-players").appendChild(li);
-    // if player count is 5 then disabling all buttons 
+    // if player count is 5 then disabling all buttons & ading a text as an alert
     const playerCount = document.getElementById("selected-players").children.length;
     if (playerCount == 5) {
         const buttons = document.getElementsByClassName("player-btn")
@@ -26,6 +26,7 @@ document.getElementById("budget-calculate").addEventListener("click", function (
     document.getElementById("player-expense").innerText = playerExpense;
 
 })
+// total cost calculation
 document.getElementById("total-calculate").addEventListener("click", function () {
     const playerExpense = parseFloat(document.getElementById("player-expense").innerText);
     const managerCost = parseFloat(document.getElementById("manager-input").value);
